c97c4aed20dc4ef0e2c03f45aabde4b54e75bc82
Eszter <esztike@acsgabi.com>
2021-05-03 13:58:43 +0200

    Create student model for frontend

---
+++ nyilvantarto/src/app/student.ts
@@ -0,0 +1,17 @@
+export class Student {
+    readonly _id: string;
+    public name: string;
+    public hourfee: number;
+    readonly balance: number = 0;
+
+    public Student() {
+        this.name = "";
+        this.hourfee = 0;
+    }
+
+    public getValidation () {
+        if (!this.name) return {isValide: false, validationMsg: "Hiányzó név"};
+        if (!this.hourfee) return {isValide: false, validationMsg: "Hiányzó óradíj"};
+        if (!Number(this.hourfee) || this.hourfee < 1) return {isValide: false, validationMsg: "Nem megfelelő formátumú óradíj"};
+    }
+}
